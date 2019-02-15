a61c3e7c82cfa5fe8ae9f3d7e617732b4f255a3b
chenzhili <1845882620@qq.com>
2019-02-15 17:24:50 +0800

    批量给 颜色缓冲区传入 数据

--- 190214.js
+++ 190214.js
@@ -21,6 +21,8 @@ window.onload = function () {
             return;
         }
 
+        const a_position = glc.getAttribLocation(glc.program,"a_position");
+        if(a_position < 0){return;}
         // 运用 webgl 的 缓存区 存储 多个顶点的信息
         const vertices = new Float32Array([0,0.5,-0.5,-0.5,0.5,-0.5]);
         const vertexBuffer = glc.createBuffer();
@@ -28,16 +30,22 @@ window.onload = function () {
         glc.bindBuffer(glc.ARRAY_BUFFER,vertexBuffer);
         glc.bufferData(glc.ARRAY_BUFFER,vertices,glc.STATIC_DRAW);
         console.log(vertices.length);
-        const a_position = glc.getAttribLocation(glc.program,"a_position");
-        if(a_position < 0){return;}
+
+        // 把缓存对象 分配给 attribute 变量
+        glc.vertexAttribPointer(a_position,2,glc.FLOAT,false,0,0);
+        // 开启 attribute 变量
+        glc.enableVertexAttribArray(a_position);
+
+        
 
         const u_fragColor = glc.getUniformLocation(glc.program,"u_fragColor");
         if(!u_fragColor){return;}
 
-        glc.vertexAttrib2f(a_position,0,0);
+        // glc.vertexAttrib2f(a_position,0,0);
         glc.uniform4f(u_fragColor,0,0,1,1);
         glc.clearColor(0, 0, 0, 1.0);
         glc.clear(glc.COLOR_BUFFER_BIT);
-        glc.drawArrays(glc.POINTS,0,1);
+        glc.drawArrays(glc.TRIANGLE_STRIP,0,3);
+        // type 的类型 glc.POINTS(点)、glc.LINES(线段)、glc.LINE_STRIP(线条，选中 点 连接的 一系列 线段 的连接)、glc.LINE_LOOP(线段的回路)、glc.TRIANGLES(一系列 三角形)、glc.TRIANGLE_STRIP(前三个点 构成第一个 三角行、从 第二个 点 开始 三个点 构成 第二个，两个三角行 共享一个边、以此类推 注意：点的 顺序 (v0,v1,v2) (v2,v1,v3))
     }
 }
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 1、对于动态 从 js 向 着色器 传值，需要分 类型；目前知道的 着色器类型有 顶点着色器 和 片源着色器
     目前知道的 向 顶点着色器 传值：需要通过 attribute,且只能用在 顶点 不能用到 片源里；
     向 片源着色器 传值：需要通过 uniform
+
+2019/2/15
+关于 webgl的原理的 url：
+http://wiki.jikexueyuan.com/project/webgl/webgL-fundamentals.html
